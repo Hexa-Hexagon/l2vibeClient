cb06ffdde64a4d6d433630e72c9f80139b9c348c
Kostjn04 <kostjn04e@gmail.com>
2023-12-21 14:00:33 +0200

    add refs to statements and text editor

--- src/components/editBlockForm/EditBlockForm.jsx
+++ src/components/editBlockForm/EditBlockForm.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "../../app.module.scss";
 import CreateNewSite from "../createNewSite/CreateNewSite";
 import Edit from "../editSite/EditSite";
 import BannerEdit from "../bannerEdit/BannerEdit";
+import {Editor} from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import EditorState from "draft-js/lib/EditorState";
 
 const EditBlockForm = ({...props}) => {
+    const [editorState, setEditorState] = useState(
+        EditorState.createEmpty()
+    );
+
+    const onEditorStateChange = (editorState) => {
+        setEditorState(editorState, {
+            allowUndo: true,
+        });
+    };
     const removeKingVip = (site) => {
         props.setKingVip(props.kingVip.filter(s => s._id !== site._id));
     };
@@ -20,28 +32,54 @@ const EditBlockForm = ({...props}) => {
     const removeStandart = (site) => {
         props.setStandart(props.standart.filter(s => s._id !== site._id));
     };
+
     return (
         <div className={classes.editBlockFormAndCreateNewSite}>
+
             <CreateNewSite addSite={props.getEdit}/>
             <div className={classes.editBlockForm}>
-                <div className={classes.editSite}>
-                    {props.kingVip.map(site => <Edit removeSite={removeKingVip} site={site}
-                                               key={site._id}/>)}
-                    {props.superVip.map(site => <Edit removeSite={removeSuperVip} site={site}
-                                                key={site._id}/>)}
-                    {props.vip.map(site => <Edit removeSite={removeVip} site={site}
-                                           key={site._id}/>)}
-                    {props.premium.map(site => <Edit removeSite={removePremium} site={site}
-                                               key={site._id}/>)}
-                    {props.standart.map(site => <Edit removeSite={removeStandart} site={site}
-                                                key={site._id}/>)}
-                </div>
-                <div className={classes.editBannerForm}>
-                    <BannerEdit banner={props.banners[0]}/>
-                    <BannerEdit banner={props.banners[1]}/>
-                    <BannerEdit banner={props.banners[2]}/>
-                    <BannerEdit banner={props.banners[3]}/>
-                    <BannerEdit banner={props.banners[4]}/>
+                {/*<div className={classes.editSite}>*/}
+                {/*    {props.kingVip.map(site => <Edit removeSite={removeKingVip} site={site}*/}
+                {/*                               key={site._id}/>)}*/}
+                {/*    {props.superVip.map(site => <Edit removeSite={removeSuperVip} site={site}*/}
+                {/*                                key={site._id}/>)}*/}
+                {/*    {props.vip.map(site => <Edit removeSite={removeVip} site={site}*/}
+                {/*                           key={site._id}/>)}*/}
+                {/*    {props.premium.map(site => <Edit removeSite={removePremium} site={site}*/}
+                {/*                               key={site._id}/>)}*/}
+                {/*    {props.standart.map(site => <Edit removeSite={removeStandart} site={site}*/}
+                {/*                                key={site._id}/>)}*/}
+                {/*</div>*/}
+                {/*<div className={classes.editBannerForm}>*/}
+                {/*    <BannerEdit banner={props.banners[0]}/>*/}
+                {/*    <BannerEdit banner={props.banners[1]}/>*/}
+                {/*    <BannerEdit banner={props.banners[2]}/>*/}
+                {/*    <BannerEdit banner={props.banners[3]}/>*/}
+                {/*    <BannerEdit banner={props.banners[4]}/>*/}
+                {/*</div>*/}
+                <div className={classes.editor}>
+                    <Editor
+                        editorState={editorState}
+                        onEditorStateChange={onEditorStateChange}
+                        toolbar={{
+                            image: {
+                                className: undefined,
+                                component: undefined,
+                                popupClassName: undefined,
+                                urlEnabled: true,
+                                uploadEnabled: true,
+                                alignmentEnabled: true,
+                                uploadCallback: undefined,
+                                previewImage: true,
+                                inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
+                                alt: { present: false, mandatory: false },
+                                defaultSize: {
+                                    height: 'auto',
+                                    width: 'auto',
+                                },
+                            },
+                        }}
+                    />
                 </div>
             </div>
         </div>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,47 +6,129 @@ import Later from "../startsLater/StartsLater";
 import JustOpened from "../justOpened/JustOpened";
 import Started from "../started/Started";
 import BonusStarted from "../bonusStarted/BonusStarted";
+import accept from "../../images/acccept.png";
 
 const Main = ({...props}) => {
     return (
         <main className={classes.main}>
-            <div className={classes.mainServers}>
-                <div className={classes.leftBlock}>
-                    <ThisWeek
-                        sites={props.startingThisWeek}
-                        starting={props.selectLan.StartingThisWeek}
-                    />
-                    <ThisMonth
-                        sites={props.startingThisMonth}
-                        starting={props.selectLan.StartingThisMonth}
-                    />
-                    <Later
-                        sites={props.startsLater}
-                        starting={props.selectLan.StartsLater}
-                    />
+            {/*<div className={classes.mainServers}>*/}
+            {/*    <div className={classes.leftBlock}>*/}
+            {/*        <ThisWeek*/}
+            {/*            sites={props.startingThisWeek}*/}
+            {/*            starting={props.selectLan.StartingThisWeek}*/}
+            {/*        />*/}
+            {/*        <ThisMonth*/}
+            {/*            sites={props.startingThisMonth}*/}
+            {/*            starting={props.selectLan.StartingThisMonth}*/}
+            {/*        />*/}
+            {/*        <Later*/}
+            {/*            sites={props.startsLater}*/}
+            {/*            starting={props.selectLan.StartsLater}*/}
+            {/*        />*/}
+            {/*    </div>*/}
+            {/*    <div className={classes.rightBlock}>*/}
+            {/*        <JustOpened*/}
+            {/*            sites={props.justOpened}*/}
+            {/*            starting={props.selectLan.JustOpened}*/}
+            {/*        />*/}
+            {/*        <Started*/}
+            {/*            sites={props.started}*/}
+            {/*            starting={props.selectLan.Started}*/}
+            {/*        />*/}
+            {/*        <BonusStarted*/}
+            {/*            sites={props.bonusStarted}*/}
+            {/*            starting={props.selectLan.BonusStarted}*/}
+            {/*        />*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<div className={classes.bannerMain}>*/}
+            {/*    {*/}
+            {/*        props.banners.map(*/}
+            {/*            banner => <a href={banner.link} key={banner._id}><input*/}
+            {/*                type="image" src={banner.banner}*/}
+            {/*                className={classes.bannerImage} alt="banner"/></a>)*/}
+            {/*    }*/}
+            {/*</div>*/}
+            <div className={classes.statements}>
+
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfsdfsdfsdfsdfssfg</a>
                 </div>
-                <div className={classes.rightBlock}>
-                    <JustOpened
-                        sites={props.justOpened}
-                        starting={props.selectLan.JustOpened}
-                    />
-                    <Started
-                        sites={props.started}
-                        starting={props.selectLan.Started}
-                    />
-                    <BonusStarted
-                        sites={props.bonusStarted}
-                        starting={props.selectLan.BonusStarted}
-                    />
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsg</a>
                 </div>
-            </div>
-            <div className={classes.bannerMain}>
-                {
-                    props.banners.map(
-                        banner => <a href={banner.link} key={banner._id}><input
-                            type="image" src={banner.banner}
-                            className={classes.bannerImage} alt="banner"/></a>)
-                }
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+                <div className={classes.statementsMain}>
+                    <img src={accept} className={classes.statementImage} alt="banner"/>
+                    <a className={classes.statementLink} href={"#"}>fgdfgsdfg</a>
+                </div>
+
+
             </div>
         </main>
     );
